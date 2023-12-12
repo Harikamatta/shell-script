09a2dbc7413b51536e1231271fee4b912b478344
Harikamatta <harikamattanjc@gmail.com>
2023-12-12 20:03:18 +0530

    second test script

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : Hello Suresh , Good Morning"
+echo "Suresh : Hello Ramesh , Good Morning"
+echo "Ramesh : Hello Suresh , How are you?"
+echo "Suresh : Hello Ramesh , I'm doing great, howv are you doing?"

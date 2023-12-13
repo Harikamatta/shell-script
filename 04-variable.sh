75991ab60a98f97d0a36e16adc26d2b7fc26daa1
Harikamatta <harikamattanjc@gmail.com>
2023-12-13 07:07:45 +0530

    adding a person

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,8 +2,10 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
 
 echo "$PERSON1 : Hello $PERSON2 , Good Morning"
 echo "$PERSON2 : Hello $PERSON1 , Good Morning"
 echo "$PERSON1 : Hello $PERSON2 , How are you?"
 echo "$PERSON2 : Hello $PERSON1 , I'm doing great, howv are you doing?"
+echo "$PERSON3 : From no where , Hi $person1 and $person2 how are u doing?"

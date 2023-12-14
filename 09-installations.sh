d3b2ec94ed175a2ebc6b8a691b0c8cf17b37099e
Harikamatta <harikamattanjc@gmail.com>
2023-12-14 16:14:54 +0530

    installing

--- 09-installations.sh
+++ 09-installations.sh
@@ -4,7 +4,9 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-echo "Error: : Please run the script with root access"
-else echo "Your root user"
-fi
+    echo "Error: : Please run the script with root access"
+    exit 1 #you can give other then 0
+else 
+    echo "Your root user"
+fi #reverse of if indicationg condition
 yum install mysql -y

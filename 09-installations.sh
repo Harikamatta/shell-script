41226e9054cafb137f74aa7b59e09c3d1bfd3596
Harikamatta <harikamattanjc@gmail.com>
2023-12-14 16:20:25 +0530

    installing

--- 09-installations.sh
+++ 09-installations.sh
@@ -10,7 +10,7 @@ else
     echo "Your root user"
 fi #reverse of if indicationg condition
 
-yum install mysqldfbzgdf -y
+yum install mysql -y
 if [ $? -ne 0 ]
 then 
     echo "Error: : Installing failed"

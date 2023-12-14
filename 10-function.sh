3ef8cb2bbb0a1004fc80c954b0d15afd54883712
Harikamatta <harikamattanjc@gmail.com>
2023-12-14 16:28:17 +0530

    functions

---
+++ 10-function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ID=$(id -u)
+VALIDATE(){
+    if [ $? -ne 0 ]
+then 
+    echo "Error: : Installing failed"
+else
+    echo "Installing is success"
+fi    
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "Error: : Please run the script with root access"
+    exit 1 #you can give other then 0
+else 
+    echo "Your root user"
+fi #reverse of if indicationg condition
+
+yum install mysql -y
+
+yum install git -y

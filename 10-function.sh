5f806d317f9c2175788741ac993bb57e7b311d8c
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 21:42:26 +0530

    functions

--- 10-function.sh
+++ 10-function.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(DATE$F-$H-$M-$S)
+TIMESTAMP=$(DATE $F-$H-$M-$S)
 R="\e[31m"
 G="e[32m"
 Y="e[33m"
@@ -12,10 +12,10 @@ echo"script started executing at $TIMESTAMP" &>>LOGFILE
 VALIDATE(){
         if [ $1 -ne 0 ]
     then 
-        echo "Error: : $2.. $RFAILED$N"
+        echo "Error: : $2.. $RFAILED $N"
         exit 1
     else
-        echo "$2.. $GSUCCESS$N"
+        echo "$2.. $GSUCCESS $N"
     fi    
 }
 
@@ -24,7 +24,7 @@ then
     echo "Error: : Please run the script with root access"
     exit 1 #you can give other then 0
 else 
-    echo "$YYour root user$N"
+    echo "$Y Your root user $N"
 fi #reverse of if indicationg condition
 
 yum install mysql -y

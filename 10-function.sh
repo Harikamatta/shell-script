244cc534cff50234a730575bad90bd9d928e4840
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 21:45:48 +0530

    functions

--- 10-function.sh
+++ 10-function.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date $F-$H-$M-$S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="e[32m"
 Y="e[33m"
 N="e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo"script started executing at $TIMESTAMP" &>>LOGFILE
+echo"script started executing at $TIMESTAMP" &>> LOGFILE
 
 VALIDATE(){
         if [ $1 -ne 0 ]

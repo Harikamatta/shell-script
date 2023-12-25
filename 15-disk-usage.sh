e3b6b1e8fe65c762e0cd1c04058b81382b53d82f
Harikamatta <harikamattanjc@gmail.com>
2023-12-25 22:37:48 +0530

    Disk usage

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -6,8 +6,8 @@ message=""
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print 6F}' | cut -d % -f1)
-    partition=$(echo $line |awk '{print 1F}')
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line |awk '{print $1F}')
     
     if [ $usage -gt $DISK_THRESHOLD ]
     then 

b43859692ec28eec44e101d9c7591e504b17473f
Harikamatta <harikamattanjc@gmail.com>
2023-12-25 19:45:10 +0530

    Delete logs

--- 13-delelete-old-logs.sh
+++ 13-delelete-old-logs.sh
@@ -6,8 +6,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ ! -d SOURCE_DIR ]
+if [ ! -d SOURCE_DIR ] #! denots opposite
 then
 echo -e "$R Source directory: $SOURCE_DIR does not exist $N"
 fi
 FILES_TO_DELETE=$(find $SOURCE_DIR  -type f -mtime +14 -name"*.log")
+
+while IFS=read -r line 
+do 
+echo "Deleting file: $line"
+
+done <<< $FILES_TO_DELETE
